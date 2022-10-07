210a2e7835d3028929de2afaf7360c0e2a1d4269
Stas <smaster950@gmail.com>
2022-10-07 15:15:55 +0300

    Complete Exercise10

--- Exercise10/Program.cs
+++ Exercise10/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int EnterNumber()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите трёхзначное число: ");
+        if (int.TryParse(Console.ReadLine(), out number))
+            break;
+        Console.WriteLine($"Ошибка ввода!");
+    }
+    return number;
+}
+
+int number = EnterNumber();
+
+int number1 = number / 10;
+int number2 = number1 % 10;
+
+Console.WriteLine($"{number} -> {number2}");
