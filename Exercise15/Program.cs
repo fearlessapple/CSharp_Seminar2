b923176ffdf9ebf8b9e8351b030c773ab7007aaa
Stas <smaster950@gmail.com>
2022-10-07 18:47:40 +0300

    Complete Exercise15

---
+++ Exercise15/Program.cs
@@ -0,0 +1,27 @@
+﻿int EnterNumber()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите день недели: ");
+        if (int.TryParse(Console.ReadLine(), out number))
+            break;
+        Console.WriteLine($"Ошибка ввода!");
+    }
+    return number;
+}
+
+int number = EnterNumber();
+
+if (number > 7)
+{
+    Console.WriteLine($"{number} -> Нет такого дня недели!");
+}
+else if (number <= 5)
+{
+    Console.WriteLine($"{number} -> Нет");
+}
+else
+{
+    Console.WriteLine($"{number} -> Да");
+}
